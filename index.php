4e57a204427ae44a5e65c9ec63733ad8ebd64e53
Michael D Mooring <Mike@mdm.cc>
2011-05-25 03:05:16 +0800

    Hello World in Noloh!

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-<?php echo "Hello world!"; ?>
+<?php
+
+
+require_once('/NOLOH/NOLOH.php');
+
+class HelloWorld extends WebPage
+
+{
+
+
+	function HelloWorld()
+
+	{
+
+
+		parent::WebPage('Hello World in NOLOH');
+
+	}
+
+}
+
+?>
