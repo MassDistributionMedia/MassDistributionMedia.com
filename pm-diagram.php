f85b0464b313db7b258dc1a17998331bed875e0e
Michael D Mooring <Mike@mdm.cc>
2011-06-01 22:09:26 +0800

    Minimized file content 5th

--- pm-diagram.php
+++ pm-diagram.php
@@ -16,9 +16,9 @@ class Moving extends WebPage
 		$this->CSSFiles->Add('http://massdistributionmedia.com/style.css');
 		$this->CSSFiles->Add('http://pm.mdm.cc/pm-style.css');
 		// Instantiate our moving panel and give it some basic visual properties: a background color and a mouse cursor
-		$this->Controls->Add($this->MovingPanel = new Panel(13, 13, 1673, 4535));
-		$this->MovingPanel->CSSClass = 'pm-diagram-bg';
-		$this->MovingPanel->Cursor = Cursor::URL("http://gif.161.be/grab.gif");
+		$this->Controls->Add($this->newPanel = new Panel(13, 13, 1673, 4535));
+		$this->newPanel->CSSClass = 'pm-diagram-bg';
+		$this->newPanel->Cursor = Cursor::URL("http://gif.161.be/grab.gif");
 		/* The following line allows the user to drag the panel around.
 		   It is read as "the MovingPanel Shifts its own Location."
 		   Whenever a Shift static (rather than Shift::With, which we'll get to later) is added to a Control's Shifts
