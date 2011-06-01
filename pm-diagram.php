2bb9ea5285d23270a7558951394f301309b3b3a6
Michael D Mooring <Mike@mdm.cc>
2011-06-02 05:44:16 +0800

    Adding mdm logo

--- pm-diagram.php
+++ pm-diagram.php
@@ -5,14 +5,15 @@ require_once('NOLOH/NOLOH.php');
 class Moving extends WebPage 
 {
 
-	private $MovingPanel, $zoomHowPanel, $ctrlImg, $mouseImg, $zoomHowLabel, $closeZoomHow, $zoomHow;
+	private $MovingPanel, $zoomHowPanel, $ctrlImg, $mouseImg, $zoomHowLabel, $closeZoomHow, $mdmLogo;
 	
 	function Moving()
 	{
 		parent::WebPage('Project Process Diagram - MDM');
 		$this->CSSFiles->Add('http://massdistributionmedia.com/style.css');
 		$this->CSSFiles->Add('http://pm.mdm.cc/pm-style.css');
-		$this->Controls->Add($this->MovingPanel = new Panel(13, 13, 1673, 4535));
+		$this->Controls->Add($this->mdmLogo = new Image("http://mdm.cc/logo-layoutv1_mdm.png", '50%', 13, null, null));
+		$this->Controls->Add($this->MovingPanel = new Panel(13, 77, 1673, 4535));
 		$this->MovingPanel->CSSClass = 'pm-diagram-bg';
 		
 		  $this->zoomHow();
@@ -20,7 +21,7 @@ class Moving extends WebPage
 	
 	   public function zoomHow()
         {
-                $this->Controls->Add($this->zoomHowPanel = new Panel(77, 77, 333, 333));
+                $this->Controls->Add($this->zoomHowPanel = new Panel('50%', '50%', 333, 333));
                 $this->zoomHow->CSSClass = 'zoomHow';
 				$this->zoomHowPanel->Controls->Add($this->mouseImg = new Image("http://gif.161.be/hand_mouse_wheel_scroll.gif", 10, 10, null, null));
                 $this->zoomHowPanel->Controls->Add($this->ctrlImg = new Image("http://png.161.be/ctrl_key.png", 13, 33, '50%', null));
@@ -28,7 +29,7 @@ class Moving extends WebPage
 				$this->zoomHowPanel->Controls->Add($this->closeZoomHow = new Button("Close"));
 				
 				$this->closeZoomHow->Click = new ServerEvent($this, 'closeZ');
-			/* $this->zoomHow->Controls->Add::Border("2pm solid pink"); */
+			    $this->zoomHowPanel->Border= "2pm solid pink";
                 $this->zoomHowPanel->Shifts[] = Shift::Location($this->zoomHowPanel);
         }
 		
