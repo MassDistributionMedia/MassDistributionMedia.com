1d0a1197435a14b9bef828052bcd069ae04431cb
Michael D Mooring <Mike@mdm.cc>
2011-06-02 21:48:50 +0800

    More Front End changes

--- pm-diagram.php
+++ pm-diagram.php
@@ -59,11 +59,11 @@ class Moving extends WebPage
 				$this->closeZoomHow->Click = new ServerEvent($this, 'closeZ');
 				
 				$this->zoomHowPanel->Controls->Add($this->nextButton = new Button("Next", 454, 267, null, null));
-				$this->closeZoomHow->BackColor= '#E1E0E0';
-				$this->closeZoomHow->Color= '#333333';
-				$this->closeZoomHow->CSSFontWeight= 'bold';
-				$this->closeZoomHow->CSSPadding= '4px';
-				$this->closeZoomHow->CSSBorderRadius= "2px";
+				$this->nextButton->BackColor= '#E1E0E0';
+				$this->nextButton->Color= '#333333';
+				$this->nextButton->CSSFontWeight= 'bold';
+				$this->nextButton->CSSPadding= '4px';
+				$this->nextButton->CSSBorderRadius= "2px";
 				/* $this->closeZoomHow->Click = new ServerEvent($this, 'closeZ'); */
 			    
         }
