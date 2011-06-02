653325f30413fb6329a831920ad0298e13f8f91d
Michael D Mooring <Mike@mdm.cc>
2011-06-02 13:31:38 +0800

    Adding style to button

--- pm-diagram.php
+++ pm-diagram.php
@@ -38,10 +38,9 @@ class Moving extends WebPage
 				$this->zoomHowPanel->Controls->Add($this->zoomHowLabel = new Label("Press Control + Scroll to Zoom."));
 				$this->zoomHowLabel->CSSFontSize= 17;
 				$this->zoomHowLabel->Width= '100%';
-				$this->zoomHowLabel->Height= 22;
+				$this->zoomHowLabel->Height= 19;
 				$this->zoomHowLabel->CSSFontWeight= 'bold';
-				$this->zoomHowLabel->CSSPadding= '5px 13px';
-				$this->zoomHowLabel->CSSMargin= '9px 13px';
+				$this->zoomHowLabel->CSSPadding= '9px 13px';
 				$this->zoomHowLabel->backColor= '#F1F1F1'; 
 				$this->zoomHowLabel->CSSDisplay= 'block'; 
 				
