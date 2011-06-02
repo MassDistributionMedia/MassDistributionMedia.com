8cf3c2e77e5ad5ac38bfce9d066b3c801869eb95
Michael D Mooring <Mike@mdm.cc>
2011-06-02 11:55:53 +0800

    Adding style to button

--- pm-diagram.php
+++ pm-diagram.php
@@ -27,7 +27,7 @@ class Moving extends WebPage
 				$this->zoomHowPanel->CSSClass = "zoomHowPanel";
 				$this->zoomHowPanel->backColor = "#fff";
 				$this->zoomHowPanel->Controls->Add($this->mouseImg = new Image("http://gif.161.be/hand_mouse_wheel_scroll.gif", 10, 10, '50%', null));
-                $this->zoomHowPanel->Controls->Add($this->ctrlImg = new Image("http://png.161.be/ctrl_key.png", 13, '27%', '27%', null));
+                $this->zoomHowPanel->Controls->Add($this->ctrlImg = new Image("http://png.161.be/ctrl_key.png", 13, '27%', 145, null));
 				$this->mouseImg->Layout= Layout::Right;
 				$this->zoomHowPanel->Controls->Add($this->zoomHowPlus = new Label("+"));
 				$this->zoomHowPlus->Layout= Layout::Center;
