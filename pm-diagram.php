e2b4bde336d1471672f5e90cd89b71386dd9ed8e
Michael D Mooring <Mike@mdm.cc>
2011-06-02 22:09:35 +0800

    More Front End changes

--- pm-diagram.php
+++ pm-diagram.php
@@ -24,7 +24,7 @@ class Moving extends WebPage
 	   public function zoomHow()
         {
                 $this->Controls->Add($this->zoomHowPanel = new Panel(222, 133, 555, 303));
-				$this->zoomHowInner->Controls->Add($this->zoomHowInner = new Panel(222, 133, 555, 303));
+				$this->zoomHowPanel->Controls->Add($this->zoomHowInner = new Panel(222, 133, 555, 303));
 				/* $this->Controls->Add($this->zoomHowInner = new Panel(222, 133, 555, 303)); */
 				$this->zoomHowInner->CSSClass = "zoomHowPanel";
 				$this->zoomHowInner->backColor = "#fff";
