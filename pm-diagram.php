2a57a501a17039345ef0963f82ce63f456049960
Michael D Mooring <Mike@mdm.cc>
2011-06-02 12:55:41 +0800

    Adding style to button

--- pm-diagram.php
+++ pm-diagram.php
@@ -40,7 +40,7 @@ class Moving extends WebPage
 				$this->zoomHowLabel->Width= '100%';
 				$this->zoomHowLabel->Height= 26;
 				$this->zoomHowLabel->CSSFontWeight= 'bold';
-				$this->zoomHowLabel->CSSMargin= '13px'; 
+				$this->zoomHowLabel->CSSMargin= '5px 10px'; 
 				$this->zoomHowPanel->Controls->Add($this->closeZoomHow = new Button("Close", null, 3, null, null));
 				$this->closeZoomHow->Layout= Layout::Right;
 				
