b3b75ebf69dbf7f994dd24fd6e67035a809d96b1
Michael D Mooring <Mike@mdm.cc>
2011-06-04 06:35:43 +0800

    Adding Reset button to Panel

--- pm-diagram.php
+++ pm-diagram.php
@@ -168,7 +168,7 @@ class Moving extends WebPage
 				$this->howResetPanel->CSSPadding= '7px';
 				
 				
-				$this->howResetPanel->Controls->Add($this->zeroKey = new Image("http://png.161.be/zero_key.png", 77, 393, null, null));
+				$this->howResetPanel->Controls->Add($this->zeroKey = new Image("http://png.161.be/zero_key.png", 393, 111, null, null));
 								
                 $this->howResetPanel->Controls->Add($this->ctrlImg = new Image("http://png.161.be/ctrl_key.png", 77, 111, 145, null));
 				$this->howResetPanel->Controls->Add($this->zoomHowPlus = new Label("+"));
