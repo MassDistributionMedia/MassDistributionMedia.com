1fac0b3e1120e1b12371650deb41112be87c4bdf
Michael D Mooring <Mike@mdm.cc>
2011-06-04 10:27:31 +0800

    Trying to add cascading

--- pm-diagram.php
+++ pm-diagram.php
@@ -148,7 +148,7 @@ class Moving extends WebPage
 				$this->mdmLabel->CSSMargin= '13px';
 				$this->mdmLabel->Color= '#777'; 
 				$this->mdmLabel->Buoyant=true;
-				$this->mdmLabel->Opacity=11;
+				$this->mdmLabel->Opacity=100;
 				$this->mdmLabel->CSSFont='Impact, Arial';
 				
 				
