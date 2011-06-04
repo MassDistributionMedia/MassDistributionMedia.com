251148cb9a96d1729bdef596d604fd979c3f98b6
Michael D Mooring <Mike@mdm.cc>
2011-06-05 00:03:23 +0800

    Adding Panel Animations

--- pm-diagram.php
+++ pm-diagram.php
@@ -60,7 +60,7 @@ class Moving extends WebPage
 			    
         }
 		
-		static function helpString($string)
+		public static function helpString($string)
 {
     $vertString = 'HELP';
     $count = strlen($string);
