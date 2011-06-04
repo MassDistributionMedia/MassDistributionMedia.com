36f83298caa28998c42e1b9a77c87bc76bdb91d4
Michael D Mooring <Mike@mdm.cc>
2011-06-04 23:22:28 +0800

    Adding Panel Animations

--- pm-diagram.php
+++ pm-diagram.php
@@ -66,7 +66,7 @@ class Moving extends WebPage
 					$this->closeZoomHow->Visible = false;
 					Animate::Property($this->howPanel, 'style.borderWidth', 0, 777);
 					Animate::Size($this->zoomHowLabel, 33, 77, 777, 555, 303);
-					$this->howPanel->howZoomPanel->zoomHowLabel->Cursor= Cursor::Hand;
+					$this->zoomHowLabel->Cursor= Cursor::Hand;
 					$this->howZoomPanel->CSSPosition = 'fixed';
 				}
 				
