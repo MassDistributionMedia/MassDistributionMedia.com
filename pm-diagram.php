d68b45d86fcd1b11cd9df9ff305139de7fb7a75e
Michael D Mooring <Mike@mdm.cc>
2011-06-10 20:16:23 +0800

    Trying to bring back the MDM Label

--- pm-diagram.php
+++ pm-diagram.php
@@ -66,18 +66,12 @@ class Moving extends WebPage
 					Animate::Location($this->howPanel, 0, 77, 777);
 					Animate::Property($this->howPanel, 'style.borderWidth', 0, 777);
 					
-				/*	$this->closeHow->Opacity = 0; */
-					Animate::Opacity($this->closeHow, 0, 777);
-				/*	$this->ctrlImg->Visible = false; */
-					Animate::Opacity($this->ctrlImg, 0, 777);
-				/*	$this->zoomHowPlus->Visible = false; */
-					Animate::Opacity($this->zoomHowPlus, 0, 777); 
-				/*	$this->nextButton->Visible = false; */
-					Animate::Opacity($this->nextButton, 0, 777);
-				/*	$this->mdmLabel->Visible = false; */
-					Animate::Opacity($this->mdmLabel, 0, 777);
-				/*	$this->mouseImg->Visible = false; */
-					Animate::Opacity($this->mouseImg, 0, 777);
+					$this->closeHow->Opacity = 0;
+					$this->ctrlImg->Visible = false;
+					$this->zoomHowPlus->Visible = false;
+					$this->nextButton->Visible = false;
+					$this->mdmLabel->Visible = false;
+					$this->mouseImg->Visible = false;
 					
 					$this->howZoomPanel->CSSPosition = 'fixed';
 					Animate::Size($this->howZoomPanel, 17, 55, 777, 777, 303);
